14a8b62ddbe87bd610cce7bf14a1d301ad534702
Austin Sloane <sloane.ats@gmail.com>
2017-10-02 12:33:12 -0700

    added chai to testing

--- test/clientSpec.js
+++ test/clientSpec.js
@@ -1,8 +1,9 @@
-var assert = require('assert');
+const expect = require('chai').expect;
+
 describe('Client Spec', function() {
   describe('#indexOf', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
 });
