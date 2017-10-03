2f6c29ef5118c341b9182c2a4b184a03fbd048ab
Laurent Frazier <laurentfrazier@gmail.com>
2017-10-02 17:23:44 -0700

    test comment in docs/feature-branch

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,2 @@
 angular.module('eventHUD').component('app', require('./app.component'));
-//This is a test of the emergency event hud system... this is only a test...
+//This is a test of the emergency event hud system... this is only a test...relax.. it's okay...you will surive the event.
