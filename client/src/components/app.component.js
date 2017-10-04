7359bc8d109944baaea6eccd16152e95c9b658ab
christine-w <christine.wong@gmail.com>
2017-10-04 12:38:29 -0700

    Add sign in form submit handler

--- client/src/components/app.component.js
+++ client/src/components/app.component.js
@@ -1,5 +1,11 @@
 module.exports = {
+  controller: function() {
+    this.signIn = (user, role) => {
+      console.log(user);
+      console.log(role);
+    };
+  },
   template: `
-    <sign-in class="sign-in"></sign-in>
+    <sign-in class="sign-in" sign-in="$ctrl.signIn"></sign-in>
 `
 };
--- client/src/components/signIn.component.js
+++ client/src/components/signIn.component.js
@@ -1,14 +1,21 @@
 module.exports = {
+  bindings: {
+    signIn: '<'
+  },
+  controller: function() {
+    this.user = '';
+    this.role = '';
+  },
   template: `
     <h1>Welcome to Event HUD!</h1>
-    <form class="signin-form">
+    <form class="signin-form" ng-submit="$ctrl.signIn($ctrl.user, $ctrl.role)">
       <div class="signin-input">
         <label for="signin-user-name">What is your name?</label>
-        <input id="signin-user-name" type="text"></input>
+        <input id="signin-user-name" type="text" ng-model="$ctrl.user"></input>
       </div>
       <div class="signin-input">
         <label for="role-select">Choose your role: </label>
-        <select id="role-select" required>
+        <select id="role-select" required ng-model="$ctrl.role">
           <option>Organizer</option>
           <option>Staff</option>
         </select>
