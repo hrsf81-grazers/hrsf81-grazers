fc2a84e2d3ae39ff8f29f9075c8575e67e5acc3c
christine-w <christine.wong@gmail.com>
2017-10-04 22:47:01 -0700

    Replace name input on sign in with a name dropdown

--- client/src/components/signIn.component.js
+++ client/src/components/signIn.component.js
@@ -2,19 +2,29 @@ module.exports = {
   bindings: {
     signIn: '<'
   },
-  controller: function() {
+  controller: function($http) {
     this.user = '';
     this.role = '';
+
+    $http({
+      method: 'GET',
+      url: `/users`
+    })
+    .then(response => response.data)
+    .then(users => this.users = users)
+    .catch(console.error);
   },
   template: `
     <h1>Welcome to Event HUD!</h1>
     <form class="signin-form" ng-submit="$ctrl.signIn($ctrl.user, $ctrl.role)">
       <div class="signin-input">
-        <label for="signin-user-name">What is your name?</label>
-        <input id="signin-user-name" type="text" ng-model="$ctrl.user"></input>
+        <label for="signin-user-name">Who are you?</label>
+        <select id="user-select" required ng-model="$ctrl.user">
+          <option ng-repeat="user in $ctrl.users track by user.id">{{user.firstName}} {{user.lastName}}</option>
+        </select>
       </div>
       <div class="signin-input">
-        <label for="role-select">Choose your role: </label>
+        <label for="role-select">What is your role?</label>
         <select id="role-select" required ng-model="$ctrl.role">
           <option>Organizer</option>
           <option>Staff</option>
