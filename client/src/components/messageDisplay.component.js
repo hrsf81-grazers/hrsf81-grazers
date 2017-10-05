0d213020e2c2092c71d81c6a560dbee6d9644342
christine-w <christine.wong@gmail.com>
2017-10-05 00:26:09 -0700

    Display all messages in event organizer message broadcast view

--- client/src/components/messageDisplay.component.js
+++ client/src/components/messageDisplay.component.js
@@ -21,7 +21,7 @@ module.exports = {
     this.receive = event => {
       console.log(`Message from the server ${event.data}`);
       const data = JSON.parse(event.data);
-      if (data.to.includes(String(this.group.id))) {
+      if (!this.group || data.to.includes(String(this.group.id))) {
         const message = this.displayMessage(data);
         document.getElementById('messages').appendChild(message);
       }
