3f3c359081652b4712bacc6c84d568c142157794
christine-w <christine.wong@gmail.com>
2017-10-08 10:28:27 -0700

    Update db config script to seed database with data

--- database/setup.js
+++ database/setup.js
@@ -1,20 +1,19 @@
 const { Pool } = require('pg');
 const config = require('./config');
+const db = require('./index');
+const seed = require('./seedData');
 
 const pool = new Pool(config);
 
 pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL UNIQUE NOT NULL PRIMARY KEY, role VARCHAR(80), firstName VARCHAR(80), lastName VARCHAR(80), email VARCHAR(80), phoneNumber VARCHAR(80))')
-.then(() =>
-
-  pool.query('CREATE TABLE IF NOT EXISTS events (id SERIAL UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(80), location VARCHAR(80), organizer_id SERIAL REFERENCES users(id))'))
-.then(() =>
-
-  pool.query('CREATE TABLE IF NOT EXISTS groups (id SERIAL UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(80), type VARCHAR(80), event_id SERIAL REFERENCES events(id))'))
-.then(() =>
-
-  pool.query('CREATE TABLE IF NOT EXISTS group_user (group_id SERIAL NOT NULL REFERENCES groups(id), user_id SERIAL NOT NULL REFERENCES users(id), PRIMARY KEY (group_id, user_id))'))
-.then(() =>
-
-  pool.query('CREATE TABLE IF NOT EXISTS messages (id SERIAL UNIQUE NOT NULL PRIMARY KEY, from_user_id SERIAL REFERENCES users(id), to_group_id SERIAL REFERENCES groups(id), title VARCHAR(80) NOT NULL, text VARCHAR(140), event_id SERIAL REFERENCES events(id), date_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP)'));
-
-
+  .then(() => pool.query('CREATE TABLE IF NOT EXISTS events (id SERIAL UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(80), location VARCHAR(80), organizer_id SERIAL REFERENCES users(id), schedule_id INTEGER)'))
+  .then(() => pool.query('CREATE TABLE IF NOT EXISTS groups (id SERIAL UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(80), type VARCHAR(80), event_id SERIAL REFERENCES events(id), schedule_id INTEGER)'))
+  .then(() => pool.query('CREATE TABLE IF NOT EXISTS group_user (group_id SERIAL NOT NULL REFERENCES groups(id), user_id SERIAL NOT NULL REFERENCES users(id), PRIMARY KEY (group_id, user_id))'))
+  .then(() => pool.query('CREATE TABLE IF NOT EXISTS messages (id SERIAL UNIQUE NOT NULL PRIMARY KEY, from_user_id SERIAL REFERENCES users(id), to_group_id SERIAL REFERENCES groups(id), title VARCHAR(80) NOT NULL, text VARCHAR(140), event_id SERIAL REFERENCES events(id), date_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP)'))
+  .then(() => Promise.all(seed.users.map(user => db.addUser(user))))
+  .then(() => Promise.all(seed.events.map(event => db.addEvent(event))))
+  .then(() => Promise.all(seed.groups.map(group => db.addGroup(group))))
+  .then(() => {
+    console.log('DB: tables created and seeded with data');
+  })
+  .catch(console.error);
