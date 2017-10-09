9ba8b9d18c384412e0c635adb426e4885180af90
christine-w <christine.wong@gmail.com>
2017-10-09 11:21:51 -0700

    Send message on message send dialog close for event organizers

--- client/src/components/messageSend.component.js
+++ client/src/components/messageSend.component.js
@@ -6,40 +6,38 @@ module.exports = {
   controller(groups, websockets, $mdDialog, $scope) {
     this.$onChanges = (changesObj) => {
       if (changesObj.group.currentValue || changesObj.user.currentValue) {
-        this.messageTitle = '';
-        this.messageBody = '';
-        this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
+        this.clearInputs();
       }
+    };
+
+    this.clearInputs = () => {
+      this.messageTitle = '';
+      this.messageBody = '';
+      this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
     };
 
     this.showSendMessageDialog = (ev) => {
       $mdDialog.show({
         controller: $scope.DialogController,
-        // locals: {
-        //   user: this.user,
-        //   messageTo: this.messageTo,
-        //   messageTitle: this.messageTitle,
-        //   messageBody: this.messageBody
-        // },
         templateUrl: 'messageSendDialog.template.html',
+        preserveScope: true,
         targetEvent: ev,
         clickOutsideToClose: true,
         escapeToClose: true
       })
-        .then(() => {
+        .then((input) => {
           this.sendMessage();
         })
         .catch(console.error);
     };
 
     $scope.DialogController = ($scope) => {
-      $scope.user = this.user;
-      $scope.messageTitle = this.messageTitle;
-      $scope.messageTo = this.messageTo;
-      $scope.messageBody = this.messageBody;
-      // console.log($scope.messageTo);
-      // console.log($scope.messageTitle);
-      // console.log($scope.messageBody);
+      $scope.msg = {
+        user: this.user,
+        messageTitle: this.messageTitle,
+        messageTo: this.messageTo,
+        messageBody: this.messageBody
+      };
 
       groups.get()
         .then((groupData) => {
@@ -52,18 +50,12 @@ module.exports = {
       };
 
       $scope.sendAndClose = () => {
-        console.log($scope.messageTo);
-        console.log($scope.messageTitle);
-        console.log($scope.messageBody);
+        this.messageTo = $scope.msg.messageTo;
+        this.messageTitle = $scope.msg.messageTitle;
+        this.messageBody = $scope.msg.messageBody;
         $mdDialog.hide();
       };
     };
-
-    // this.clearInputs = () => {
-    //   this.messageTitle = '';
-    //   this.messageBody = '';
-    //   this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
-    // };
 
     this.sendMessage = () => {
       const toGroupIds = [];
@@ -74,7 +66,7 @@ module.exports = {
         toGroupNames.push(toGroup[1]);
       });
       websockets.send(JSON.stringify({
-        toNames: toGroupNames,
+        recipients: toGroupNames,
         toIds: toGroupIds,
         title: this.messageTitle,
         text: this.messageBody,
