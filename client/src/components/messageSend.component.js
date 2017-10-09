fca8e7efa271504ab33bf9ee0d817c00f9eb20ac
christine-w <christine.wong@gmail.com>
2017-10-09 09:53:33 -0700

    Replace message send form as a dialog

--- client/src/components/messageSend.component.js
+++ client/src/components/messageSend.component.js
@@ -3,24 +3,67 @@ module.exports = {
     user: '<',
     group: '<'
   },
-  controller(groups, websockets) {
+  controller(groups, websockets, $mdDialog, $scope) {
     this.$onChanges = (changesObj) => {
       if (changesObj.group.currentValue || changesObj.user.currentValue) {
-        this.clearInputs();
+        this.messageTitle = '';
+        this.messageBody = '';
+        this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
       }
     };
 
-    groups.get()
-      .then((groupData) => {
-        this.groups = groupData;
+    this.showSendMessageDialog = (ev) => {
+      $mdDialog.show({
+        controller: $scope.DialogController,
+        // locals: {
+        //   user: this.user,
+        //   messageTo: this.messageTo,
+        //   messageTitle: this.messageTitle,
+        //   messageBody: this.messageBody
+        // },
+        templateUrl: 'messageSendDialog.template.html',
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        escapeToClose: true
       })
-      .catch(console.error);
-
-    this.clearInputs = () => {
-      this.messageTitle = '';
-      this.messageBody = '';
-      this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
+        .then(() => {
+          this.sendMessage();
+        })
+        .catch(console.error);
     };
+
+    $scope.DialogController = ($scope) => {
+      $scope.user = this.user;
+      $scope.messageTitle = this.messageTitle;
+      $scope.messageTo = this.messageTo;
+      $scope.messageBody = this.messageBody;
+      // console.log($scope.messageTo);
+      // console.log($scope.messageTitle);
+      // console.log($scope.messageBody);
+
+      groups.get()
+        .then((groupData) => {
+          $scope.groups = groupData;
+        })
+        .catch(console.error);
+
+      $scope.hide = () => {
+        $mdDialog.hide();
+      };
+
+      $scope.sendAndClose = () => {
+        console.log($scope.messageTo);
+        console.log($scope.messageTitle);
+        console.log($scope.messageBody);
+        $mdDialog.hide();
+      };
+    };
+
+    // this.clearInputs = () => {
+    //   this.messageTitle = '';
+    //   this.messageBody = '';
+    //   this.messageTo = this.user.role === 'organizer' ? [] : [JSON.stringify([this.group.id, this.group.name])];
+    // };
 
     this.sendMessage = () => {
       const toGroupIds = [];
